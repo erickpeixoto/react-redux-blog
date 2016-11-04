2df6309845cb052aa3f30ab11b7ca65bed37d726
__@erick <erickeduardo@systemroot.com.br>
2016-11-04 00:01:51 -0200

    Loading data on render

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,13 +1,33 @@
 import React,{Component} from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsShow extends Component {
 
+    componentWillMount(){
+        this.props.fetchPost(this.props.params.id);
+    }
+
     render() {
+      const { post } = this.props;
+
+        if(!post){
+          return (
+              <div className="class-name">Loading...</div>
+          );
+        }
         return (
             <div className="class-name">
-                Post Show
+                <Link to="/">Back To index</Link>
+                <h3>{ post.title }</h3>
+                <h6>Categories: { post.categories }</h6>
+                <p>{ post.content }</p>
             </div>
         );
     }
 }
-export default PostsShow;
+function mapStateToProps(state){
+  return { post: state.posts.post }
+}
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
